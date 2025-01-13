e0a8dbf652cb26179bf5182c12db4d0061a4f03f
DivyanshuxOP <opxdivyanshu@gmail.com>
2025-01-13 21:26:54 +0530

    first commit

---
+++ front/src/shared.js
@@ -0,0 +1,9 @@
+let link = '';
+
+export const setLink = (newLink) => {
+    link = newLink;
+};
+
+export const getLink = () => {
+    return link;
+};
--- src/shared.js
+++
@@ -1,9 +0,0 @@
-let link = '';
-
-export const setLink = (newLink) => {
-    link = newLink;
-};
-
-export const getLink = () => {
-    return link;
-};
